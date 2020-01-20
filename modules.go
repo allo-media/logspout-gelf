8b5af46c4e0db57d5746b778f848c1be25aa2602
Merouane Atig <merwan@patem.net>
2020-01-20 14:26:35 +0100

    Use logspout-gelf module

--- modules.go
+++ modules.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "github.com/looplab/logspout-logstash"
+	_ "github.com/micahhausler/logspout-gelf"
 
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
